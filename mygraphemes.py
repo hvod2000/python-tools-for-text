2d9a22a4017c0f1f8b7dac47f2780119746a660d
Uladzislau Khamkou <kham@tuta.io>
2022-01-15 16:43:24 +0300

    refactoring

--- mygraphemes.py
+++ mygraphemes.py
@@ -3,15 +3,10 @@ from wcwidth import wcswidth as get_width
 def graphemes(text):
     grapheme = ""
     for code in text:
-        if get_width(code) == 0:
-            grapheme += code
-        elif get_width(code) > 0:
+        if get_width(code) != 0:
             if grapheme:
                 yield grapheme
-            grapheme = code
-        else:
-            if grapheme:
-                yield grapheme
-            grapheme = code
+            grapheme = ""
+        grapheme += code
     if grapheme:
         yield grapheme
